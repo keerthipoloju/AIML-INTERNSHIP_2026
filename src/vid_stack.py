1670187b31a53a44d1fe9e06a30d55e9a1023b27
keerthipoloju <keerthipoloju32@gmail.com>
2026-05-27 11:10:25 +0530

    Updated project with generic paths

--- src/vid_stack.py
+++ src/vid_stack.py
@@ -1,30 +1,32 @@
 import cv2
 import numpy as np
 
-cap1 = cv2.VideoCapture(r'C:\Users\keert\OneDrive\Documents\computer vision\2.mp4')
-cap2 = cv2.VideoCapture(r'C:\Users\keert\OneDrive\Documents\computer vision\2.mp4')
+cap1 = cv2.VideoCapture("data/video1.mp4")
+cap2 = cv2.VideoCapture("data/video2.mp4")
 
 if not cap1.isOpened() or not cap2.isOpened():
-    print("Error: Could not open video.")
+    print("Error: Could not open videos.")
     exit()
 
 while True:
+
     ret1, frame1 = cap1.read()
     ret2, frame2 = cap2.read()
 
     if not ret1 or not ret2:
         break
 
-    frame1 = cv2.resize(frame1, (640, 360))
-    frame2 = cv2.resize(frame2, (640, 360))
+    frame1 = cv2.resize(frame1, (640,360))
+    frame2 = cv2.resize(frame2, (640,360))
 
-    h_concat = np.hstack((frame1, frame2))
+    stacked = np.hstack((frame1, frame2))
 
-    cv2.imshow('Concatenated Video', h_concat)
+    cv2.imshow("Stacked Video", stacked)
 
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
 
 cap1.release()
 cap2.release()
+
 cv2.destroyAllWindows()
